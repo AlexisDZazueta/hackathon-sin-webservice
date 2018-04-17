01a6f0f797560d6e9c9b15c5971a6b33de51f82d
Alexis D. Zazueta <azazueta@MacBook-Pro-de-Alexis.local>
2018-04-17 17:13:27 -0600

    Add user routes to the API

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,14 @@ const express = require('express');
 const userCtrl = require('../controllers/user');
 //const eventCtrl = require('../controllers/event');
 //const locationCtrl = require('../controllers/location');
+const auth = require('../middlewares/auth');
 const api = express.Router();
 
-// API Routes
-
+// Users Routes
+api.post('/users/signup', userCtrl.signUp);
+api.post('/users/signin', userCtrl.signIn);
+api.put('/users/:userId', auth, userCtrl.updateUser);
+api.delete('/users/:userId', auth, userCtrl.deleteUser);
 
 // Export modules
 module.exports = api;
